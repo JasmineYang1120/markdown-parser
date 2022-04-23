969f70b1de6521eb249b18bfa107108bf4652090
JasmineYang1120 <103228511+JasmineYang1120@users.noreply.github.com>
2022-04-23 16:55:30 -0700

    aa

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,28 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-    assertEquals(2, 1 + 1);
+        assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void test1() throws IOException{
+        String file = Files.readString(Path.of("test-file.md"));
+        ArrayList<String> links1 = MarkdownParse.getLinks(file);
+        assertEquals(List.of("https://something.com","some-thing.html"),
+        links1);
+    }
+    @Test
+    public void test2() throws IOException{
+        String file1 = Files.readString(Path.of("test-file1.md"));
+        ArrayList<String> links2 = MarkdownParse.getLinks(file1);
+        assertEquals(List.of("some-thing.html"),links2);
     }
 }
