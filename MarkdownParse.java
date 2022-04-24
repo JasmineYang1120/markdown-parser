ad3dae47e057a8a910797bf9ac481333f065096c
JasmineYang1120 <103228511+JasmineYang1120@users.noreply.github.com>
2022-04-23 17:15:12 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,6 +18,11 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            if (closeParen == markdown.length()-2){
+                if (openBracket==0){
+                    break;
+                }
+            }
 
         }
         return toReturn;
